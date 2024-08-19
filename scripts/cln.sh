7f29e9a8572885b783d9eac11828fa02e7cb3045
Milos Milosavljevic <milosh@emisia.net>
2024-08-19 14:08:19 +0200

    Netplan

--- scripts/cln.sh
+++ scripts/cln.sh
@@ -3,4 +3,8 @@
 sed -i -e '/GRUB_CMDLINE_LINUX/ s:"$: net.ifnames=0":' /etc/default/grub
 /sbin/grub-mkconfig -o /boot/grub/grub.cfg
 
+# Arm64
 sed -ie 's/enp0s3/eth0/' /etc/netplan/50-cloud-init.yaml
+
+# Amd64
+sed -ie 's/ens3/eth0/' /etc/netplan/50-cloud-init.yaml
