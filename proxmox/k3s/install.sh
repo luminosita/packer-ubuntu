8e71793ef23814038186a635b4274ae40693a4dd
Milos Milosavljevic <milosh@emisia.net>
2024-09-03 17:08:50 +0200

    user

--- proxmox/k3s/install.sh
+++ proxmox/k3s/install.sh
@@ -4,6 +4,8 @@
 IP_ADDRESSES=($HOSTS)  
 # Define the k3s version  
 K3S_VERSION="v1.24"  
+
+USER="ubuntu"
   
 # Check if there is at least one IP address  
 if [ ${#IP_ADDRESSES[@]} -eq 0 ]; then  
@@ -14,7 +16,7 @@ fi
 # Install the master node  
 MASTER_IP=${IP_ADDRESSES[0]}  
 echo "Installing master node: $MASTER_IP"  
-k3sup install --ip $MASTER_IP --user addo --k3s-channel $K3S_VERSION \  
+k3sup install --ip $MASTER_IP --user $USER --k3s-channel $K3S_VERSION \  
     --k3s-extra-args '--write-kubeconfig-mode 644 --write-kubeconfig ~/.kube/config --flannel-backend=none --cluster-cidr=\"10.100.0.0/16\" --disable traefik --disable metrics-server --disable local-storage --disable-network-policy' \  
     --local-path /tmp/config  
   
@@ -23,7 +25,7 @@ for i in "${!IP_ADDRESSES[@]}"; do
     if [ $i -ne 0 ]; then  
         AGENT_IP=${IP_ADDRESSES[$i]}  
         echo "Installing agent node: $AGENT_IP"  
-        k3sup join --ip $AGENT_IP --server-ip $MASTER_IP --user addo --k3s-channel $K3S_VERSION  
+        k3sup join --ip $AGENT_IP --server-ip $MASTER_IP --user $USER --k3s-channel $K3S_VERSION  
     fi  
 done  
   
